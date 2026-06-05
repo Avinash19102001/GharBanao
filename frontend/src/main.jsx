986409d9a7f12a0ce49230c76cbd8dfcdbdd36c1
AkarapuBhargava <147802110+AkarapuBhargava@users.noreply.github.com>
2026-06-05 12:15:46 +0530

    add:house-owner registration

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-import { RouterProvider } from 'react-router-dom'
-import routes from './routes/AppRouter.jsx'
+import { RouterProvider } from "react-router-dom";
+import routes from "./routes/AppRouter.jsx";
+import HouseOwnerProfile from "./components/auth/HouseOwnerProfile.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={routes} />
+    {/* <RouterProvider router={routes} /> */}
+    <HouseOwnerProfile />
   </StrictMode>,
-)
+);
