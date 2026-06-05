b31a00895b19211e91db0ebd12581c6cb89fc82c
Vaishnavi <padarajuvaishnavi@gmail.com>
2026-06-05 12:28:12 +0530

    supplier registration

---
+++ backend/app/schemas/supplier_registration.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+class SupplierCreate(BaseModel):
+    full_name: str
+    email: str
+    password: str
+    confirm_password: str
+    company_name: str
+
+class SupplierLogin(BaseModel):
+    email: str
+    password: str
