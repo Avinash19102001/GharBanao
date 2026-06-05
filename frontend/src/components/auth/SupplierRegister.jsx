1d67db40575b5e3f960c09c1ccbd913a957575ec
Sampangi Uday Kumar <uday@Sampangis-MacBook-Air.local>
2026-06-05 19:04:18 +0530

    Updated supplier registration and routes

--- frontend/src/components/auth/SupplierRegister.jsx
+++ frontend/src/components/auth/SupplierRegister.jsx
@@ -1,10 +1,9 @@
-
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { supplierSchema } from "./schemas/supplierSchema";
 import { useNavigate } from "react-router-dom";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
-import { useState} from "react";
+import { useState } from "react";
 
 
 const SupplierRegister = () => {
