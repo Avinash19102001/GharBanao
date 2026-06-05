c2660b2f116955702f4f759ed47d169331933fc6
PraveenKumarDoasni <dosanipraveenkumar4@gmail.com>
2026-06-05 11:10:07 +0530

    house-owner related changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.routers.contractor_router import router
+from app.routers.house_owner_register import router as house_owner_router
 
 app = FastAPI()
 
@@ -10,3 +11,4 @@ def home():
     }
 
 app.include_router(router)
+app.include_router(house_owner_router)
