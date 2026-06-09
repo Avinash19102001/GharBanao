28ac036d10a1ec80c60756f9f0aebf7aecd738a1
Ramyasri033 <baswaramyasri25@gmail.com>
2026-06-09 12:52:38 +0530

    updated database.py

--- backend/app/utils/database.py
+++ backend/app/utils/database.py
@@ -1,21 +1,25 @@
 from dotenv import load_dotenv
 import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# DATABASE_URL = (
-#     "postgresql+psycopg2://postgres:123@localhost:5432/gharbanao"
-# )
- #DATABASE_URL = (
- #    "postgresql+psycopg2://postgres:vyshu%401234@localhost:5432/gharbanao"
- #)
-
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = (
+    f"postgresql+psycopg2://{DB_USERNAME}:{DB_PASSWORD}"
+    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
+print(DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
-
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -24,7 +28,6 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
